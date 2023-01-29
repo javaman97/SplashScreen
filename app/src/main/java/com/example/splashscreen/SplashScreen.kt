bbbfb2db1e53ef7fe0bdade19d251e22c88c03b6
aman nishad <amannishad981@gmail.com>
2023-01-29 12:38:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.example.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        //USED to hide status bar and make it as full screen activity
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
